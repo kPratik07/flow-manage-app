7641ed2cefd24dedd997312043e0997fdeefc6ad
PRATIK RAJ <kpratik071997@gmail.com>
2025-09-24 20:00:54 +0530

    issues fixed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compare } from "bcryptjs";
-import { prisma } from "@/lib/db";
+import { prisma } from "../../../../lib/db";
 
 const handler = NextAuth({
     providers: [
--- app/api/auth/signin/route.ts
+++ app/api/auth/signin/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { prisma } from '@/lib/db'
+import { prisma } from "../../../../lib/db";
 import { verifyPassword, generateToken } from '@/lib/auth'
 import { signInSchema } from '@/utils/validation'
 
